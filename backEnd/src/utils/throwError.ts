cc9fd46ce5f165e1b9f7f549dcac7ffcbab8b91f
ThVinicius <vinicioss9955@gmail.com>
2022-12-28 17:17:47 -0300

    feat: throwError utils

---
+++ backEnd/src/utils/throwError.ts
@@ -0,0 +1,23 @@
+export function badRequest(message: string) {
+  throw { code: 'Bad request', message }
+}
+
+export function unauthorized(message: string) {
+  throw { code: 'Unauthorized', message }
+}
+
+export function notFound(message: string) {
+  throw { code: 'Not Found', message }
+}
+
+export function conflit(message: string) {
+  throw { code: 'Conflit', message }
+}
+
+export function upgradeRequired(message: string) {
+  throw { name: 'Upgrade Required', message }
+}
+
+export function notAcceptable(message: string) {
+  throw { code: 'Not Acceptable', message }
+}
