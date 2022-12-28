7da4c23da9361268b91dbf1dcc64200f360f5475
ThVinicius <vinicioss9955@gmail.com>
2022-12-28 17:11:46 -0300

    feat: add routes and errorHandling

--- backEnd/src/app.ts
+++ backEnd/src/app.ts
@@ -1,9 +1,15 @@
 import express, { json } from 'express'
+import 'express-async-errors'
 import cors from 'cors'
+import { errorHandling } from './middlewares/errorHandling'
+import routes from './routes/index'
 
 const app = express()
 
 app.use(cors())
 app.use(json())
+
+app.use(routes)
+app.use(errorHandling)
 
 export default app
