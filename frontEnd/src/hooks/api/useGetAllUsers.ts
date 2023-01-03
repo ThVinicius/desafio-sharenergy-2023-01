996c3eea42eb115dab7104b6b9db7b2044a8eebd
ThVinicius <vinicioss9955@gmail.com>
2023-01-03 17:51:00 -0300

    feat: login persistence

--- frontEnd/src/hooks/api/useGetAllUsers.ts
+++ frontEnd/src/hooks/api/useGetAllUsers.ts
@@ -1,15 +1,24 @@
 import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
+import { useToken } from '../../context/useToken'
 import { IUser } from '../../types/User'
+import loginPersistence from '../../utils/loginPersistence'
 
 export default function useGetAllUsers() {
+  const navigate = useNavigate()
+  const { token: tokenContext, setToken } = useToken()
   const [allUsers, setAllUsers] = useState<IUser[] | null>(null)
   const [loadingAllUsers, setLoadingAllUsers] = useState(true)
 
   useEffect(() => {
+    const token = loginPersistence(tokenContext, setToken, navigate)
+
     const URL = `${import.meta.env.VITE_BASE_URL}/random-users`
 
-    const promise = axios.get(URL)
+    const config = { headers: { authorization: `Bearer ${token}` } }
+
+    const promise = axios.get(URL, config)
 
     promise
       .then(({ data }) => setAllUsers(data))
