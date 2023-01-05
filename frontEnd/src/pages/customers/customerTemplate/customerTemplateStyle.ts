e2d71faff5f6e9a2d510d10834db62162ffe227c
ThVinicius <vinicioss9955@gmail.com>
2023-01-05 18:15:35 -0300

    refactor: rename

--- frontEnd/src/pages/customers/customerTemplate/customerTemplateStyle.ts
+++ frontEnd/src/pages/customers/customerTemplate/customerTemplateStyle.ts
@@ -26,38 +26,10 @@ const Info = styled.div`
   }
 `
 
-const Address = styled.div`
-  width: 100%;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  gap: 5px 0;
-
-  h4 {
-    font: normal 700 20px 'Lexend Deca', sans-serif;
-  }
-`
-
-const AddressBox = styled.div`
-  width: 50%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-
-  h5 {
-    font: normal 700 18px 'Lexend Deca', sans-serif;
-  }
-
-  h6 {
-    font: normal 300 17px 'Lexend Deca', sans-serif;
-    text-align: center;
-  }
-`
-
 const ButtonsBox = styled.div`
   width: 100%;
   display: flex;
   justify-content: space-between;
 `
 
-export { Container, Info, Address, AddressBox, ButtonsBox }
+export { Container, Info, ButtonsBox }
