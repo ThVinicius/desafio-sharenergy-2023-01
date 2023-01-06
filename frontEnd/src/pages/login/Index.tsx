ee6a1ffd3405cc5cf6f616f958e3dd026c376c81
ThVinicius <vinicioss9955@gmail.com>
2023-01-06 10:08:06 -0300

    feat: loginPersistence

--- frontEnd/src/pages/login/Index.tsx
+++ frontEnd/src/pages/login/Index.tsx
@@ -1,8 +1,13 @@
-import { FC } from 'react'
+import { FC, useEffect } from 'react'
 import AppContainer from '../../containers/alignCenter/AlignCenter'
+import usePersistence from '../../hooks/usePersistence'
 import Form from './form/Form'
 
 const Login: FC = () => {
+  const { loginPersistence } = usePersistence()
+
+  useEffect(() => loginPersistence(), [])
+
   return (
     <AppContainer>
       <Form />
